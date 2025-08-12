aa8757baf43ce49e7640f5c1734f8c92681fd190
Dimar Tamara <dimartamara1@gmail.com>
2025-08-12 14:18:11 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Deskripsi from './homepage/deskripsi/page'
 import Skills from './homepage/skills/page'
 
 export default function Home() {
+  // selesai
   return (
     <div className="w-full bg-[black] h-auto flex justify-center">
       <div className="max-[942px]:w-full items-center flex flex-col gap-2 w-[947px] h-auto px-3 z-50">
